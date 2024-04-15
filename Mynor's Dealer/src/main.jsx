2a5945e4d35ba175144468775a301f589947cada
Mynor <Kapatano3005@gmail.com>
2024-04-15 08:23:19 -0400

    Add NextUIProvider to main.jsx and update dependencies in package.json

--- Mynor's Dealer/src/main.jsx
+++ Mynor's Dealer/src/main.jsx
@@ -1,7 +1,11 @@
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './main.css'
+import { NextUIProvider } from "@nextui-org/react";
+
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-    <App />
+    <NextUIProvider>
+        <App />
+    </NextUIProvider>
 )
--- Mynor's Dealer/tailwind.config.js
+++ Mynor's Dealer/tailwind.config.js
@@ -1,27 +1,34 @@
+
+/* eslint-env node */
+const { nextui } = require("@nextui-org/react");
+const { Scroll } = require("tailwind-scrollbar");
+
 /** @type {import('tailwindcss').Config} */
 export default {
   content: [
     "./index.html",
     "./src/**/*.{js,ts,jsx,tsx}",
+    "./node_modules/@nextui-org/theme/dist/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
     extend: {},
     fontFamily: {
       'poppins': ['Poppins', 'sans-serif'],
     },
-      colors:{
-        "WhiteMynor": "#FFFFFE",
-        "BlueMynor": "#100E34",
-        "RedMynor": "#a2001d",
-        "GrayMynor1": "#F8F8F8",
-        "GrayMynor2": "#EAEBF0",
-        "GrayMynor3": "#ABF1BA",
-        "GrayMynor4": "#666872",
-      }
+    darkMode: "class",
+    colors: {
+      "WhiteMynor": "#FFFFFE",
+      "BlueMynor": "#100E34",
+      "RedMynor": "#a2001d",
+      "GrayMynor1": "#F8F8F8",
+      "GrayMynor2": "#EAEBF0",
+      "GrayMynor3": "#ABF1BA",
+      "GrayMynor4": "#666872",
+    }
   },
   plugins: [
-    /* eslint-env node */
-    require('tailwind-scrollbar'),
+    nextui(),
+    Scroll()
   ],
 }
 
